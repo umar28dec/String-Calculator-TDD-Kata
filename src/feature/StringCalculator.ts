d4dc58cfbe0f8b47d7f4ef074e759c863d2203ce
Umar Farooque Khan <uffarooque73@gmail.com>
2024-12-04 15:41:50 +0530

    Implement handling for single number input: returns the number itself (e.g., "1" -> 1, "2" -> 2)

--- src/feature/StringCalculator.ts
+++ src/feature/StringCalculator.ts
@@ -2,6 +2,7 @@ export class StringCalculator {
   add(numbers: string): number {
     let sum = 0;
     if (!numbers) return sum;
+    sum += parseInt(numbers);
     return sum;
   }
 }
--- test/StringCalculator.test.ts
+++ test/StringCalculator.test.ts
@@ -10,4 +10,12 @@ describe("StringCalculator", () => {
   test("should return 0 for an empty string", () => {
     expect(calculator.add("")).toBe(0);
   });
+
+  it('should return 1 for input "1"', () => {
+    expect(calculator.add("1")).toBe(1);
+  });
+
+  it('should return 8 for input "81"', () => {
+    expect(calculator.add("8")).toBe(8);
+  });
 });
