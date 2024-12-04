a64d72b867dfeaa73f74ae809c9e6fd00e61c5cb
Umar Farooque Khan <uffarooque73@gmail.com>
2024-12-04 13:34:11 +0530

    Handle empty string input in add() method, return 0

---
+++ src/feature/StringCalculator.ts
@@ -0,0 +1,7 @@
+export class StringCalculator {
+  add(numbers: string): number {
+    let sum = 0;
+    if (!numbers) return sum;
+    return sum;
+  }
+}
---
+++ test/StringCalculator.test.ts
@@ -0,0 +1,13 @@
+import { StringCalculator } from "../src/feature/StringCalculator";
+
+describe("StringCalculator", () => {
+  let calculator: StringCalculator;
+
+  beforeEach(() => {
+    calculator = new StringCalculator();
+  });
+
+  test("should return 0 for an empty string", () => {
+    expect(calculator.add("")).toBe(0);
+  });
+});
